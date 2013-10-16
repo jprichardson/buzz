3247e89a1005bb569297b2e0cae7743c6f566d6b
JP Richardson <jprichardson@gmail.com>
2013-10-16 14:04:47 -0500

    added more tests

--- test/cmd.test.js
+++ test/cmd.test.js
@@ -12,4 +12,37 @@ describe('+ cmd.splitArgs', function() {
       EQ (argO.execName, 'my-script.sh')
     })
   })
+
+  describe('> when only one param is passed and the command has a parameter', function() {
+    it('should properly identify the command and its args', function() {
+      var args = "node buzz -d 250 my-script.sh 10".split(' ')
+      var argO = cmd.splitArgs(args)
+      EQ (argO.n, 4)
+      EQ (argO.buzzArgs, 'node,buzz,-d,250')
+      EQ (argO.execArgs, '10')
+      EQ (argO.execName, 'my-script.sh')
+    })
+  })
+
+  describe('> when no params are passed and the command has a parameter', function() {
+    it('should properly identify the command and its args', function() {
+      var args = "node buzz my-script.sh 10".split(' ')
+      var argO = cmd.splitArgs(args)
+      EQ (argO.n, 2)
+      EQ (argO.buzzArgs, 'node,buzz')
+      EQ (argO.execArgs, '10')
+      EQ (argO.execName, 'my-script.sh')
+    })
+  })
+
+  describe('> when no params are passed', function() {
+    it('should properly identify the command and its args', function() {
+      var args = "node buzz my-script.sh".split(' ')
+      var argO = cmd.splitArgs(args)
+      EQ (argO.n, 2)
+      EQ (argO.buzzArgs, 'node,buzz')
+      EQ (argO.execArgs, '')
+      EQ (argO.execName, 'my-script.sh')
+    })
+  })
 })
